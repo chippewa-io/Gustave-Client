28d8c346869efbccc7dbaa8b99baf30571373abd
Chris <m.christopher.cohoon@me.com>
2023-07-18 11:32:11 -0500

    Updated binary to read config
    URL for gustave server was hardcoded to gustave.chippewa.io.  Updated to gather the URL from the /Library/Managed\ Preferences/io.chippewa.gustave.plist file.

--- gustave/gustave/services.swift
+++ gustave/gustave/services.swift
@@ -10,101 +10,13 @@ import IOKit
 import os.log
 
 class Services {
-    let db = Database()
-    let gustaveServerURL = "https://gustave.chippewa.io" // Replace with your Gustave server URL
-    let log = OSLog(subsystem: "io.chippewa.gustave", category: "Services")
-    let spinner = Spinner()
-    
-    func generateSecret(spinner: Spinner, silent: Bool = false) {
-        var secretGenerationCompleted = false
-        DispatchQueue.global(qos: .background).async {
-                while !secretGenerationCompleted {
-                    if !silent {
-                        // Update the spinner
-                        spinner.tick()
-                        usleep(200000) // sleep for 200 milliseconds
-                    }
-                }
-                if !silent {
-                    // Stop the spinner when done
-                    spinner.stop()
-                }
-            }
-            //print("getting secret...")
-            os_log(.default, log: log, "getting secret...")
-            
-            if db.getUnexpiredSecret() != nil {
-                //print("Using existing secret.")
-                os_log(.default, log: log, "Using existing secret.")
-                return
-            }
-            //print("No pre-existing secret found")
-            os_log(.default, log: log, "No pre-existing secret found.")
-            let udid = getUDID()
-            let url = URL(string: "\(gustaveServerURL)/api/secret")!
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST"
-            request.httpBody = "udid=\(udid)".data(using: .utf8)
-
-            let semaphore = DispatchSemaphore(value: 0)  // 1. Create a semaphore
-            
-        let task = URLSession.shared.dataTask(with: request) { [self] data, response, error in
-                guard let data = data, error == nil else {
-                    //print("Error: \(error?.localizedDescription ?? "Unknown error")")
-                    os_log(.error, log: log, "Unknown error: %{public}@", error?.localizedDescription ?? "Unknown error")
-                    return
-                }
-                
-                if let _ = String(data: data, encoding: .utf8) {
-                    let secret = self.getSecretFromPlist()
-                    self.storeSecretInDatabase(secret: secret)
-                    self.checkSecretStatus(secret: secret)
-                    semaphore.signal()// 3. Signal the semaphore when the task is done
-                } else {
-                    //print("Failed to generate a secret.")
-                    os_log(.error, log: log, "Failed to generate a secret: %{public}@", error?.localizedDescription ?? "No error description available")
-                }
-            }
-
-            task.resume()
-
-            semaphore.wait()  // 2. Wait for the semaphore to be signaled before returning from the function
-        self.deleteProfileWithUDID(silent: silent, secretGenerationCompleted: &secretGenerationCompleted)
-                }
-    
-    func updateComputer(id: String, value: String) {
-        //print("Updating computer...")
-        os_log(.default, log: log, "Updating computer...." )
-        let udid = getUDID()
-        let secretData = db.getMostRecentSecret()
-        guard let secret = secretData?.secret else {
-            //print("No secrets found in the database.")
-            os_log(.default, log: log, "No secrets found in the database." )
-            return
-        }
-        let url = URL(string: "\(gustaveServerURL)/api/computers/update")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        let json: [String: Any] = ["udid": udid, "secret": secret, "id": id, "value": value]
-        let jsonData = try? JSONSerialization.data(withJSONObject: json)
-        request.httpBody = jsonData
-        let semaphore = DispatchSemaphore(value: 0) // 1. Create a semaphore
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            defer { semaphore.signal() } // 3. Signal the semaphore when the task is done
-            guard let data = data, error == nil else {
-                //print("Error: \(error?.localizedDescription ?? "Unknown error")")
-                os_log(.error, log: self.log, "Error: %{public}@", error?.localizedDescription ?? "Unknown error")
-                return
-            }
-            if let responseJSON = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                print(responseJSON)
-                os_log(.default, log: self.log, "Response from Jamf Pro: %{public}@", responseJSON.description)
-            }
-        }
-        task.resume()
-        semaphore.wait() // 2. Wait for the semaphore to be signaled before returning from the function
-    }
+    lazy var gustaveServerURL: String? = {
+            return UserDefaults(suiteName: "io.chippewa.gustave")?.string(forKey: "GustaveServerURL")
+        }()
+        
+        let db = Database()
+        let log = OSLog(subsystem: "io.chippewa.gustave", category: "Services")
+        let spinner = Spinner()
 
 
     func getUDID() -> String {
@@ -118,71 +30,7 @@ class Services {
         }
         return ""
     }
-    
-    func deleteProfileWithUDID(silent: Bool = false, secretGenerationCompleted: inout Bool) {
-            let UDID = getUDID()
-            guard !UDID.isEmpty else {
-                //print("Can't get UDID.  Is this a Virtual Machine?")
-                os_log(.default, log: log, "Can't get UDID.  Is this a Virtual Machine?  Gustave does not support virtual machines without proper and unique UDIDs." )
-                return
-            }
-            let semaphore = DispatchSemaphore(value: 0)
-            let urlString = "\(gustaveServerURL)/api/profiles?udid=\(UDID)"
-            guard let url = URL(string: urlString) else {
-                //print("Invalid URL.")
-                os_log(.default, log: log, "Invalid Gustave URL.  Check to see if the Gustave Configuration Profile has been installed" )
-                return
-            }
 
-            var request = URLRequest(url: url)
-            request.httpMethod = "DELETE"
-            
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                if let error = error {
-                    //print("Error: \(error)")
-                    os_log(.error, log: self.log, "Error: %{public}@", error.localizedDescription)
-                } else if let httpResponse = response as? HTTPURLResponse {
-                    if !silent {
-                                // Stop the spinner when done
-                        self.spinner.stop()
-                        var secretGenerationCompleted = true
-                                
-                            }
-                    //print("Response status code: \(httpResponse.statusCode)")
-                    os_log(.default, log: self.log, "Response status code: %d", httpResponse.statusCode)
-                }
-                semaphore.signal()
-            }
-            
-            task.resume()
-            semaphore.wait()
-        
-        }
-
-    func storeSecretInDatabase(secret: String) {
-        let expiration = getExpirationDate()
-        db.insertSecret(secret: secret, expiration: expiration)
-        //print("Secret stored.")
-        os_log(.default, log: self.log, "Secret stored")
-    }
-
-    func getExpirationDate() -> String {
-        let plistPath = "/Library/Managed Preferences/io.chippewa.gustave.plist"
-        guard let plistData = FileManager.default.contents(atPath: plistPath) else {
-            return ""
-        }
-        
-        var format = PropertyListSerialization.PropertyListFormat.xml
-        guard let plistDict = try? PropertyListSerialization.propertyList(from: plistData, options: .mutableContainersAndLeaves, format: &format) as? [String: Any],
-              let secretDict = plistDict["Secret"] as? [String: Any],
-              let expirationDate = secretDict["Expiration"] as? Int else {
-            return ""
-        }
-        
-        return String(expirationDate)
-    }
-
-    
     func getSecretFromPlist(maxAttempts: Int = 5) -> String {
         let regexPattern = "^[a-fA-F0-9]{32}$"  // Replace with your actual regex pattern
         let defaults = UserDefaults(suiteName: "io.chippewa.gustave")
@@ -222,7 +70,185 @@ class Services {
         return secret
     }
 
+    func deleteProfileWithUDID(silent: Bool = false, secretGenerationCompleted: inout Bool) {
+        guard let gustaveServerURL = gustaveServerURL else {
+            print("Gustave Config File not readable... please ensure you've deployed the gustave config.mobileconfig file properly with Jamf Pro")
+            return
+        }
 
+        let UDID = getUDID()
+        guard !UDID.isEmpty else {
+            //print("Can't get UDID.  Is this a Virtual Machine?")
+            os_log(.default, log: log, "Can't get UDID.  Is this a Virtual Machine?  Gustave does not support virtual machines without proper and unique UDIDs." )
+            return
+        }
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        let urlString = "\(gustaveServerURL)/api/profiles?udid=\(UDID)"
+        guard let url = URL(string: urlString) else {
+            //print("Invalid URL.")
+            os_log(.default, log: log, "Invalid Gustave URL.  Check to see if the Gustave Configuration Profile has been installed" )
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        
+        var localSecretGenerationCompleted = secretGenerationCompleted  // create a local copy
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                //print("Error: \(error)")
+                os_log(.error, log: self.log, "Error: %{public}@", error.localizedDescription)
+            } else if let httpResponse = response as? HTTPURLResponse {
+                if !silent {
+                    // Stop the spinner when done
+                    self.spinner.stop()
+                    localSecretGenerationCompleted = true
+                }
+                //print("Response status code: \(httpResponse.statusCode)")
+                os_log(.default, log: self.log, "Response status code: %d", httpResponse.statusCode)
+            }
+            semaphore.signal()
+        }
+        
+        task.resume()
+        semaphore.wait()
+        
+        secretGenerationCompleted = localSecretGenerationCompleted  // assign it back to the inout parameter
+    }
+
+    func generateSecret(spinner: Spinner, silent: Bool = false) {
+        var secretGenerationCompleted = false
+        DispatchQueue.global(qos: .background).async {
+            while !secretGenerationCompleted {
+                if !silent {
+                    // Update the spinner
+                    spinner.tick()
+                    usleep(200000) // sleep for 200 milliseconds
+                }
+            }
+            if !silent {
+                // Stop the spinner when done
+                spinner.stop()
+            }
+        }
+        
+        os_log(.default, log: log, "getting secret...")
+                
+        if db.getUnexpiredSecret() != nil {
+            os_log(.default, log: log, "Using existing secret.")
+            print("Already initialized.")
+            return
+        }
+        
+        os_log(.default, log: log, "No pre-existing secret found.")
+        
+        let udid = getUDID()
+
+        // Safely handle gustaveServerURL and url
+        guard let gustaveServerURL = gustaveServerURL else {
+            os_log(.error, log: log, "GustaveServerURL not found.")
+            return
+        }
+        
+        let urlString = "\(gustaveServerURL)/api/secret"
+        guard let url = URL(string: urlString) else {
+            os_log(.error, log: log, "Invalid URL: %{public}@", urlString)
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = "udid=\(udid)".data(using: .utf8)
+
+        let semaphore = DispatchSemaphore(value: 0)  // 1. Create a semaphore
+                
+        let task = URLSession.shared.dataTask(with: request) { [self] data, response, error in
+            guard let data = data, error == nil else {
+                os_log(.error, log: log, "Unknown error: %{public}@", error?.localizedDescription ?? "Unknown error")
+                return
+            }
+                    
+            if let _ = String(data: data, encoding: .utf8) {
+                let secret = self.getSecretFromPlist()
+                self.storeSecretInDatabase(secret: secret)
+                self.checkSecretStatus(secret: secret)
+                semaphore.signal()// 3. Signal the semaphore when the task is done
+                secretGenerationCompleted = true  // Set the secretGenerationCompleted flag to true
+            } else {
+                os_log(.error, log: log, "Failed to generate a secret: %{public}@", error?.localizedDescription ?? "No error description available")
+            }
+        }
+
+        task.resume()
+        semaphore.wait()  // 2. Wait for the semaphore to be signaled before returning from the function
+        self.deleteProfileWithUDID(silent: silent, secretGenerationCompleted: &secretGenerationCompleted)
+    }
+
+
+
+    func updateComputer(id: String, value: String) {
+        //print("Updating computer...")
+        os_log(.default, log: log, "Updating computer...." )
+        let udid = getUDID()
+        let secretData = db.getMostRecentSecret()
+        guard let secret = secretData?.secret else {
+            //print("No secrets found in the database.")
+            os_log(.default, log: log, "No secrets found in the database." )
+            return
+        }
+        if let gustaveServerURL = gustaveServerURL {
+            let url = URL(string: "\(gustaveServerURL)/api/computers/update")!
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            let json: [String: Any] = ["udid": udid, "secret": secret, "id": id, "value": value]
+            let jsonData = try? JSONSerialization.data(withJSONObject: json)
+            request.httpBody = jsonData
+            let semaphore = DispatchSemaphore(value: 0) // 1. Create a semaphore
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                defer { semaphore.signal() } // 3. Signal the semaphore when the task is done
+                guard let data = data, error == nil else {
+                    //print("Error: \(error?.localizedDescription ?? "Unknown error")")
+                    os_log(.error, log: self.log, "Error: %{public}@", error?.localizedDescription ?? "Unknown error")
+                    return
+                }
+                if let responseJSON = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                    print(responseJSON)
+                    os_log(.default, log: self.log, "Response from Jamf Pro: %{public}@", responseJSON.description)
+                }
+            }
+            task.resume()
+            semaphore.wait() // 2. Wait for the semaphore to be signaled before returning from the function
+        } else {
+            print("Gustave Config File not readable... please ensure you've deployed the gustave config.mobileconfig file properly with Jamf Pro")
+        }
+    }
+
+
+    func storeSecretInDatabase(secret: String) {
+        let expiration = getExpirationDate()
+        db.insertSecret(secret: secret, expiration: expiration)
+        //print("Secret stored.")
+        os_log(.default, log: self.log, "Secret stored")
+    }
+
+    func getExpirationDate() -> String {
+        let plistPath = "/Library/Managed Preferences/io.chippewa.gustave.plist"
+        guard let plistData = FileManager.default.contents(atPath: plistPath) else {
+            return ""
+        }
+        
+        var format = PropertyListSerialization.PropertyListFormat.xml
+        guard let plistDict = try? PropertyListSerialization.propertyList(from: plistData, options: .mutableContainersAndLeaves, format: &format) as? [String: Any],
+              let secretDict = plistDict["Secret"] as? [String: Any],
+              let expirationDate = secretDict["Expiration"] as? Int else {
+            return ""
+        }
+        
+        return String(expirationDate)
+    }
 
     func checkSecretStatus(secret: String) {
         let expirationDate = getExpirationDate()
@@ -232,7 +258,7 @@ class Services {
             //print("Secret is not active or expired.")
         }
     }
-    
+
     func queryComputer() {
         //print("Querying computer...")
         let udid = getUDID()
@@ -241,6 +267,12 @@ class Services {
             //print("No secrets found in the database.")
             return
         }
+        
+        guard let gustaveServerURL = gustaveServerURL else {
+            print("Gustave Config File not readable... please ensure you've deployed the gustave config.mobileconfig file properly with Jamf Pro")
+            return
+        }
+        
         let url = URL(string: "\(gustaveServerURL)/api/computers")!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
@@ -266,6 +298,4 @@ class Services {
 
         semaphore.wait()  // 2. Wait for the semaphore to be signaled before returning from the function
     }
-
-
 }
